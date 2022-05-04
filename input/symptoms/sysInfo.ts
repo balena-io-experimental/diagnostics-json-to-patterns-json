4aa42995f57152ceaa54d27f81b05b474bd139a3
fisehara <harald@balena.io>
2022-05-04 22:57:36 +0200

    updated sysInfo extractregex
    Signed-off-by: fisehara <harald@balena.io>

--- input/symptoms/sysInfo.ts
+++ input/symptoms/sysInfo.ts
@@ -6,17 +6,17 @@ export default {
 		extractSupervisorVersion: {
 			description: 'Extracting Supervisor version with a regex',
 			$$formula:
-				'REGEXEXTRACT(contract[\'journalctl --no-pager --no-hostname -n 200 -a -u balena\'].stdout, "Supervisor (v[0-9]+.[0-9]+.[0-9]+).starting up")',
+				"REGEXEXTRACT(contract['balena logs {id}'].stdout, 'Supervisor (v[0-9]+.[0-9]+.[0-9]+).starting up')",
 		},
 		extractLinuxKernelVersion: {
 			description: 'Extracting Linux kernel version with regex',
 			$$formula:
-				'REGEXEXTRACT(contract[\'journalctl --no-pager --no-hostname -n 200 -a -u balena\'].stdout, "Linux version ([0-9]+.[0-9]+.[0-9]+)")',
+				"REGEXEXTRACT(contract['dmesg -T'].stdout, 'Linux version ([0-9]+.[0-9]+.[0-9]+)')",
 		},
 		extractBalenaOsVersion: {
 			description: 'Extracting Linux kernel version with regex',
 			$$formula:
-				'REGEXEXTRACT(contract[\'journalctl --no-pager --no-hostname -n 200 -a -u balena\'].stdout, "balenaOS ([0-9]+.[0-9]+.[0-9]+.rev[0-9]+)")',
+				"REGEXEXTRACT(contract['cat /etc/os-release'].stdout, 'balenaOS ([0-9]+.[0-9]+.[0-9]+.rev[0-9]+)')",
 		},
 	},
 } as JsonSchema;
